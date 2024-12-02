a38f5e84d644987549f684e14afc68eb2710cff8
pappik <paparao.prema75@gmail.com>
2024-12-02 12:58:30 -0500

    add instnce profile to aws instance

--- module/main.tf
+++ module/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "instance" {
   ami                    = data.aws_ami.expense_ami.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
-#  iam_instance_profile = aws_iam_role.
+  iam_instance_profile = aws_iam_instance_profile.instance_profile.name
 
   tags = {
     Name = var.tool_name
