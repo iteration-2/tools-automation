dd580eff34b606850784023231d03991c1fc48a4
pappik <paparao.prema75@gmail.com>
2024-12-02 12:50:39 -0500

    adding policy to role

--- main.tf
+++ main.tf
@@ -4,8 +4,9 @@ module "tools" {
   for_each      = var.tools
 
 
-  tool_name     = each.key
-  instance_type = each.value["instance_type"]
+  tool_name            = each.key
+  instance_type        = each.value["instance_type"]
+  policy_resource_list = each.value["policy_resource_list"]
 
   zone_id       = var.zone_id
 }
--- variables.tf
+++ variables.tf
@@ -2,10 +2,17 @@ variable "tools" {
   default = {
     prometheus = {
       instance_type = "t3.small"
+      policy_resource_list = ["ec2:DescribeInstances"]
     }
   }
 }
 
 variable "zone_id" {
   default = "Z0569018TEBR9GQ8VL2K"
+}
+
+variable "policy_resource_list" {
+  default = {
+
+  }
 }
