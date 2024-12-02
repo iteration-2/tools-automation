90acf976d40d7a2679c6234a8a4b76f7bc3271e4
pappik <paparao.prema75@gmail.com>
2024-12-02 12:41:54 -0500

    aws iam role and instance profile addition

--- module/main.tf
+++ module/main.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "instance" {
   ami                    = data.aws_ami.expense_ami.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+#  iam_instance_profile = aws_iam_role.
 
   tags = {
     Name = var.tool_name
@@ -14,4 +15,30 @@ resource "aws_route53_record" "record" {
   zone_id = var.zone_id
   records = [aws_instance.instance.public_ip]
   ttl = 30
+}
+
+resource "aws_iam_role" "role" {
+  name = "${var.tool_name}-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    Name = "${var.tool_name}-role"
+  }
+}
+
+resource "aws_iam_instance_profile" "instance_profile" {
+name = "${var.tool_name}-role"
+role = aws_iam_role.role.name
 }
